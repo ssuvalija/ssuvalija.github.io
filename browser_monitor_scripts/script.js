0dbbcf9c4bf83c9f6fb2c2d67c93a3807a8749ab
Selma Šuvalija <62721493+ssuvalija@users.noreply.github.com>
2021-10-01 13:43:54 +0200

    Update script.js

--- browser_monitor_scripts/script.js
+++ browser_monitor_scripts/script.js
@@ -1,1 +1,7 @@
-text
+async function testPage(page) {
+  // visit the home page
+  await page.goto('\''https://www.apple.com/'\'');
+  // take screenshot of the home page
+  await page.screenshot({ path: '\''screenshot.jpg'\'' });
+}
+module.exports = testPage;
