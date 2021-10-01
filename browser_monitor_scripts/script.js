b2baa4481616b74010430430089b41973ccf585a
Selma Šuvalija <62721493+ssuvalija@users.noreply.github.com>
2021-10-01 13:25:59 +0200

    Update script.js

--- browser_monitor_scripts/script.js
+++ browser_monitor_scripts/script.js
@@ -1,10 +1,1 @@
-async function testPage(page) {
-      //update script test
-      // visit the home pagee
-      await page.goto('https://www.github.com/');
-      
-      // take screenshot of the home page
-      await page.screenshot({ path: 'screenshot.jpg' });
-    }
-    
-module.exports = testPage;
+async function testPage(page)
