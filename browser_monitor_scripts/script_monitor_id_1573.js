1cc98098504b7482050a817eeb86bf192d0482fe
Selma Šuvalija <62721493+ssuvalija@users.noreply.github.com>
2021-12-21 14:14:41 +0100

    Update script_monitor_id_1573.js

--- browser_monitor_scripts/script_monitor_id_1573.js
+++ browser_monitor_scripts/script_monitor_id_1573.js
@@ -1,1 +1,8 @@
-test7
+// This script loads the webpage and takes a screenshot of the loaded page.
+async function testPage(page) {
+  // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
+  await page.goto("https://ssuvalija.github.io/index.html#");
+  
+  await page.screenshot({ path: 'screenshot.jpg' });
+}
+module.exports = testPage;
