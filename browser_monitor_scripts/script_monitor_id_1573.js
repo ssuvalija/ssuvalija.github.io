19cc4f039e9335c3ec94865a6766a395b2030217
Selma Suvalija <selma.suvalija@sematext.com>
2021-12-21 21:18:55 +0100

    use secrets

--- browser_monitor_scripts/script_monitor_id_1573.js
+++ browser_monitor_scripts/script_monitor_id_1573.js
@@ -2,9 +2,13 @@
 async function testPage(page) {
   // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
   await page.goto("https://ssuvalija.github.io/index.html#");
+  await page.goto("https://ssuvalija.github.io/index.html#");
+
 
   await page.screenshot({ path: 'screenshot.jpg' });
 }
 module.exports = testPage;
+
+
 
 
