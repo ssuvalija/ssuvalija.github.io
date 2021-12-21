dca5abf6f04253165cf9f1d203478da42e392b66
Selma Suvalija <selma.suvalija@sematext.com>
2021-12-21 15:31:26 +0100

    updated workflow

--- browser_monitor_scripts/script_monitor_id_1573.js
+++ browser_monitor_scripts/script_monitor_id_1573.js
@@ -2,6 +2,8 @@
 async function testPage(page) {
   // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
   await page.goto("https://ssuvalija.github.io/index.html#");
+  await page.goto("https://ssuvalija.github.io/index.html#");
+
   
   await page.screenshot({ path: 'screenshot.jpg' });
 }
