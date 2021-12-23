c958a2d05ed646e3162905da0755b721bdc10aeb
Selma Suvalija <selma.suvalija@sematext.com>
2021-12-23 15:43:59 +0100

    update script

--- browser_monitor_scripts/script_monitor_id_1573.js
+++ browser_monitor_scripts/script_monitor_id_1573.js
@@ -1,11 +1,13 @@
 //appId=3981;monitorId=1153,1189
 async function testPage(page) {
   // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
-  await page.goto("https://sematext.com/");
+  await page.goto("https://sematext.com/docs");
 
   await page.screenshot({ path: 'screenshot.jpg' });
 }
 module.exports = testPage;
+
+
 
 //sync both scripts
 
