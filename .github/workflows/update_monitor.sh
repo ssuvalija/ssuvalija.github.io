231e8f53cc80eb17e10386efa00504d81a308fcd
Selma Suvalija <selma.suvalija@sematext.com>
2021-12-24 08:25:15 +0100

    update script

--- .github/workflows/update_monitor.sh
+++ .github/workflows/update_monitor.sh
@@ -4,38 +4,38 @@ file='test-script.js'
 
 # (echo "#monitorId=67667868" && cat 'test-script.js') > filename1 && mv filename1 'test-script.js'
 
-errors=()
-not_synced=""
-line=$( head -n 1 $file )
-IFS=';' read -r -a ids <<< "$line"
+# errors=()
+# not_synced=""
+# line=$( head -n 1 $file )
+# IFS=';' read -r -a ids <<< "$line"
 
-ids_length=${#ids[@]}
-if [ $ids_length != 2 ]; then
-    echo "App ID and monitor IDs must be specified on the firts line using the format //appId=1;monitorId=2,3"
-    not_synced+=${file}'; '
-fi
+# ids_length=${#ids[@]}
+# if [ $ids_length != 2 ]; then
+#     echo "App ID and monitor IDs must be specified on the firts line using the format //appId=1;monitorId=2,3"
+#     not_synced+=${file}'; '
+# fi
 
-echo $not_synced
+# echo $not_synced
 
-IFS='=' read -r -a app_id <<< "${ids[0]}"
-app_id=${app_id[1]}
+# IFS='=' read -r -a app_id <<< "${ids[0]}"
+# app_id=${app_id[1]}
 
-app_id_length=${#app_id[@]}
-if [ $app_id_length != 2 ]; then
-    errors+="new error"
-    echo "App ID not fouind. App ID and monitor IDs must be specified on the firts line using the format //appId=1;monitorId=2,3"
-fi
+# app_id_length=${#app_id[@]}
+# if [ $app_id_length != 2 ]; then
+#     errors+="new error"
+#     echo "App ID not fouind. App ID and monitor IDs must be specified on the firts line using the format //appId=1;monitorId=2,3"
+# fi
 
-echo "============"
-echo $errors
+# echo "============"
+# echo $errors
 
-IFS='=' read -r -a monitor_ids <<< "${ids[1]}"
-monitor_ids=${monitor_ids[1]}
+# IFS='=' read -r -a monitor_ids <<< "${ids[1]}"
+# monitor_ids=${monitor_ids[1]}
 
-monitor_ids_length=${#monitor_ids[@]}
-if [ $monitor_ids_length != 2 ]; then
-    echo "Monitor ID not found. App ID and monitor IDs must be specified on the firts line using the format //appId=1;monitorId=2,3"
-fi
+# monitor_ids_length=${#monitor_ids[@]}
+# if [ $monitor_ids_length != 2 ]; then
+#     echo "Monitor ID not found. App ID and monitor IDs must be specified on the firts line using the format //appId=1;monitorId=2,3"
+# fi
 
 
 script_content=$( base64 $file)
@@ -49,17 +49,17 @@ api_url+='/synthetics-api/api/v3/apps/3981/monitors/browser/1153'
 auth_header='Authorization: apiKey '
 auth_header+='55d4a7c1-4d9f-44e5-be42-1a5e42be62bc'
 
-# echo $api_url
+ echo $api_url
 
-# echo $auth_header
+ echo $auth_header
 
 # echo "++++++++++++++++++++++++"
 # echo ${api_url}
 
 curl --location --request PUT $api_url \
---header $auth_header \
+--header 'Authorization: apiKey 55d4a7c1-4d9f-44e5-be42-1a5e42be62bc' \
 --header 'Content-Type: application/json' \
---data '{"script":"'$script_content'"}' > result.txt
+--data '{"script":"'$script_content'","scriptBased":true}' > result.txt
 
 cat result.txt
 
--- browser_monitor_scripts/script_monitor_id_1573.js
+++ browser_monitor_scripts/script_monitor_id_1573.js
@@ -16,8 +16,3 @@ module.exports = testPage;
 
 
 
-
-
-
-
-
