eb1e125504d30634bbc99bd52d4698b47a340fc0
Selma Suvalija <selma.suvalija@sematext.com>
2021-12-24 16:13:41 +0100

    remove scripts

--- .github/workflows/test-script copy.js
+++
@@ -1,9 +0,0 @@
-// This script loads the webpage and takes a screenshot of the loaded page.
-async function testPage(page) {
-  // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
-  await page.goto("https://ssuvalija.github.io/index.html#");
-  await page.goto("https://ssuvalija.github.io/elements.html");
-  
-  await page.screenshot({ path: 'screenshot.jpg' });
-}
-module.exports = testPage;
--- .github/workflows/test-script.js
+++
@@ -1,9 +0,0 @@
-//appId=;monitorId=xxxtTODO
-async function testPage(page) {
-  // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
-  await page.goto("https://selma.github.io/index.html#");
-  await page.goto("https://selma.github.io/elements.html");
-  
-  await page.screenshot({ path: 'screenshot.jpg' });
-}
-module.exports = testPage;
