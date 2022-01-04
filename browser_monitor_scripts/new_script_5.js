58314f08f740f726b9e8b618dd049a4a3e8b1b08
Selma Suvalija <selma.suvalija@sematext.com>
2022-01-04 16:08:39 +0100

    add new file

--- browser_monitor_scripts/new_script_5.js
+++ browser_monitor_scripts/new_script_5.js
@@ -1,4 +1,4 @@
-//appId=3981;monitorId=
+//appId=3981
 async function testPage(page) {
   // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
   await page.goto("https://ssuvalija.github.io/index.html");
