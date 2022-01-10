710337127096a93d6f2a2f4cc4eaea1ea3523b38
Selma Suvalija <selma.suvalija@sematext.com>
2022-01-10 14:06:09 +0100

    added script

--- browser_monitor_scripts/new_script_36666.js
+++ browser_monitor_scripts/new_script_36666.js
@@ -3,6 +3,8 @@ async function testPage(page) {
   // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
   await page.goto("https://ssuvalija.github.io/index.html");
 
+  await page.goto("https://ssuvalija.github.io/index.html");
+
   await page.screenshot({ path: 'screenshot.jpg' });
 }
 module.exports = testPage;
