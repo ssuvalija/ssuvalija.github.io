01028ae102e7883451f52b2b361a1d21d41b7333
Selma Suvalija <selma.suvalija@sematext.com>
2022-01-12 21:08:53 +0100

    test merge

--- browser_monitor_scripts/my new test copy 2.js
+++ browser_monitor_scripts/my new test copy 2.js
@@ -1,12 +1,7 @@
-//appId=3981;monitorId=1230
+//appId=3981
 async function testPage(page) {
   // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
   await page.goto("https://ssuvalija.github.io/index.html");
-
-  await page.goto("https://ssuvalija.github.io/index.html");
-
-  await page.goto("https://ssuvalija.github.io/index.html");
-
 
   await page.screenshot({ path: 'screenshot.jpg' });
 }
