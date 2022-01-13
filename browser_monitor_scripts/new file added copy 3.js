7ad87f0411321288c66c7069509aea7d4b81ab07
Selma Suvalija <selma.suvalija@sematext.com>
2022-01-13 13:27:57 +0100

    first commit

---
+++ browser_monitor_scripts/new file added copy 3.js
@@ -0,0 +1,13 @@
+//appId=3981
+async function testPage(page) {
+  // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
+  await page.goto("https://ssuvalija.github.io/index.html");
+
+  await page.screenshot({ path: 'screenshot.jpg' });
+}
+module.exports = testPage;
+
+
+//sync both scripts
+
+
