03d4b763732701e929796f4883eccaa83b1f840c
Selma Suvalija <selma.suvalija@sematext.com>
2022-01-13 13:35:31 +0100

    second commit

--- browser_monitor_scripts/new_script_32.js
+++ browser_monitor_scripts/new_script_32.js
@@ -1,6 +1,10 @@
 //appId=3981;monitorId=1226
 async function testPage(page) {
   // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
+    // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
+
+      // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
+
   await page.goto("https://ssuvalija.github.io/index.html");
 
   await page.screenshot({ path: 'screenshot.jpg' });
