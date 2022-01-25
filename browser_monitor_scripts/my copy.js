5e214ff4238e767ae562bf19d23e4b89b57837e8
Selma Suvalija <selma.suvalija@sematext.com>
2022-01-25 16:43:13 +0100

    update script

--- browser_monitor_scripts/my copy.js
+++ browser_monitor_scripts/my copy.js
@@ -1,6 +1,7 @@
 //appId=11557
 async function testPage(page) {
   // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
+  //update
   await page.goto("https://ssuvalija.github.io/index.html");
 
   await page.screenshot({ path: 'screenshot.jpg' });
