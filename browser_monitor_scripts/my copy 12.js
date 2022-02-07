4dc7e3e1166551906f5a2e242544f35d514e39b5
Selma Suvalija <selma.suvalija@sematext.com>
2022-02-07 12:18:37 +0100

    add new script with only app Id

---
+++ browser_monitor_scripts/my copy 12.js
@@ -0,0 +1,12 @@
+//appId=11557
+async function testPage(page) {
+  // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
+  //update    
+  //new update
+  await page.goto("https://ssuvalija.github.io/index.html");
+
+  await page.screenshot({ path: 'screenshot.jpg' });
+}
+module.exports = testPage;
+
+//newest workflow test 2
