68ffc739ffd074c049ff3172fa2347159a514b2e
Selma Suvalija <selma.suvalija@sematext.com>
2022-02-07 12:35:28 +0100

    add new script with both  app Id and monitor ID

--- browser_monitor_scripts/my copy 14.js
+++ browser_monitor_scripts/my copy 14.js
@@ -1,4 +1,4 @@
-//appId=11557; monitorId=2712
+//appId =  11557; monitorId  = 2712
 async function testPage(page) {
   // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
   //update    
@@ -9,4 +9,4 @@ async function testPage(page) {
 }
 module.exports = testPage;
 
-//newest workflow test 2
+//update test
