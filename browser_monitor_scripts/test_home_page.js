2407fbea1c03f3ab057df3c0e6bb9f3d5af3dd92
Selma Suvalija <selma.suvalija@sematext.com>
2022-02-09 17:29:57 +0100

    update multiple scripts

--- browser_monitor_scripts/test_home_page.js
+++ browser_monitor_scripts/test_home_page.js
@@ -1,12 +1,7 @@
 //appId=15920;monitorId=2725
 async function testPage(page) {
-  // Replace YOUR_WEBPAGE_URL with URL of the webpage to be monitored
-  //update    
-  //new update
-  await page.goto("https://ssuvalija.github.io/index.html");
+  await page.goto("https://apps.eu.sematext.com/");
 
   await page.screenshot({ path: 'screenshot.jpg' });
 }
 module.exports = testPage;
-
-//newest workflow test 2 hhh  jjj    
